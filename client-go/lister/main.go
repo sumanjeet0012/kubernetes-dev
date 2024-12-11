967576202c70587d096d624981ace6040cd60045
Sumanjeet <sumanjeet0012@gmail.com>
2024-12-11 15:52:49 +0530

    lister is made from little modification from one

---
+++ client-go/lister/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	kubeconfig := flag.String("kubeconfig", "/home/sumanjeet/.kube/config", "absolute path to the kubeconfig file")
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		fmt.Printf("error %s building config from flags", err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Printf("error %s creating clientset", err.Error())
+	}
+	// fmt.Println(clientset)
+
+	pods, err := clientset.CoreV1().Pods("default").List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Printf("error %s listing pods", err.Error())
+	}
+
+	fmt.Println("The pods in the default namespace are:")
+	for _, pod := range pods.Items {
+		fmt.Println(pod.Name)
+	}
+	// fmt.Println(pods)
+
+	deployments, err := clientset.AppsV1().Deployments("default").List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Printf("error %s listing deployments", err.Error())
+	}
+
+	fmt.Println("The deployments in the default namespace are:")
+	for _, deployment := range deployments.Items {
+		fmt.Println(deployment.Name)
+	}
+
+}
